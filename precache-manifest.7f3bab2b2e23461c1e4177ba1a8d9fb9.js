91888a4182225df58973dbb629e340eb9f3ffd14
jecrs687 <emanuelcascone@gmail.com>
2020-02-18 14:54:11 +0100

    Updates

---
+++ precache-manifest.7f3bab2b2e23461c1e4177ba1a8d9fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5cadfb79c7596bdf11d7f13ec96cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a34690f6483b0e0b71",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aa138a718215b9747370",
+    "url": "/static/js/2.372740d6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.372740d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a34690f6483b0e0b71",
+    "url": "/static/js/main.3c2d02e8.chunk.js"
+  },
+  {
+    "revision": "bb6e999808b72a46598d",
+    "url": "/static/js/runtime-main.cb5f7b1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
