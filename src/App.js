5a8284b2688badb7eda49ae9e5d32e39487369dc
jecrs687 <emanuelcascone@gmail.com>
2020-02-18 15:00:57 +0100

    site created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>The most common words in any language </p>
+    </div>
+  );
+}
+
+export default App;
