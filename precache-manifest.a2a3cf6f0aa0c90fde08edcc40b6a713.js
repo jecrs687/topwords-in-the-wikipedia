719a1df0d58c311c6662f0ffaab4dcc7448c730f
jecrs687 <emanuelcascone@gmail.com>
2020-02-21 14:21:50 +0100

    Updates

---
+++ precache-manifest.a2a3cf6f0aa0c90fde08edcc40b6a713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01c1e64b4625d8913b79d95e6bb219d",
+    "url": "/topwords-in-the-wikipedia/index.html"
+  },
+  {
+    "revision": "2ca025e7039224287d45",
+    "url": "/topwords-in-the-wikipedia/static/css/main.cd4d6f37.chunk.css"
+  },
+  {
+    "revision": "dca76feed248ad3441d3",
+    "url": "/topwords-in-the-wikipedia/static/js/2.088a3f21.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/topwords-in-the-wikipedia/static/js/2.088a3f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca025e7039224287d45",
+    "url": "/topwords-in-the-wikipedia/static/js/main.0c8b15a8.chunk.js"
+  },
+  {
+    "revision": "d58f6a498a50ecbab6c4",
+    "url": "/topwords-in-the-wikipedia/static/js/runtime-main.8581074e.js"
+  }
+]);
